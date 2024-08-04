a010eba3764ff64494d6b75d9fe2d85f62bd0f3d
Denis Romodanov <darvenommm@gmail.com>
2024-08-05 00:01:59 +0300

    feat: add a validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 
 import { ConfigurationModule } from '@infrastructure/configuration';
 import { DatabaseModule } from '@infrastructure/database';
 import { GraphqlModule } from '@infrastructure/graphql';
 import { LibraryModule } from '@modules/library';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [ConfigurationModule, DatabaseModule, GraphqlModule, LibraryModule],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        transform: true,
+        forbidNonWhitelisted: true,
+      }),
+    },
+  ],
 })
 export class AppModule {}
